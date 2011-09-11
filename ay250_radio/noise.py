e517d73fe7a98fd6a4e4adaaa61278ba0f14ddc3
James McBride <jmcbride@berkeley.edu>
2011-09-11 14:59:06 -0500

    added arguments to noise

--- ay250_radio/noise.py
+++ ay250_radio/noise.py
@@ -2,3 +2,24 @@
 # should take beam size and wavelength as arguments
 # should predict noise levels for observations of given bandwidth, time,
 # number of antennae, etc.
+
+
+
+def noise(bandwidth, intTime, beamSize, wavelength):
+    '''
+    Purpose
+    -------
+
+    Inputs
+    ------
+
+    Notes
+    -----
+
+    '''
+
+    numSamp = bandwidth * intTime
+
+    return 0
+
+
