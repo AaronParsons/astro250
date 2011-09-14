ca2b34d70afc2abf699f2bb14894ac1405deeb8a
Aaron Parsons <aparsons@astron.berkeley.edu>
2011-09-14 16:36:34 -0700

    Refactored noise function to attach to Telescope class.

--- ay250_radio/noise.py
+++ ay250_radio/noise.py
@@ -33,6 +33,41 @@ class Telescope:
     def setDependent(self):
 
         self.effArea = self.numAnt * self.areaDish * self.effAper
+    def noise(self, bandwidth, intTime, wavelength):
+        '''
+        Purpose
+        -------
+        This provides the expected noise level in Jansky of a telescope,
+        given the observing parameters and properties of the telescope. 
+
+        Inputs
+        ------
+        bandwidth: float
+            Bandwidth in Hz
+        intTime: float
+            Integration time in seconds
+
+        wavelength: 
+
+        Returns
+        -------
+        fluxDensityRMS: float
+            The rms in the flux density, in units of Janskys 
+
+
+        Notes
+        -----
+        Maybe supposed to develop more of the variables used here?
+
+        '''
+
+        numSamp = bandwidth * intTime
+        gain = (self.effArea / (2 * kb) ) * jansky
+
+        rmsTemp = self.sysTemp / math.sqrt(numSamp)
+        rmsFluxDensity = rmsTemp / gain
+
+        return rmsFluxDensity
 
 class ATA(Telescope):
     def __init__(self):
@@ -45,44 +80,5 @@ class CARMA(Telescope):
         Telescope.__init__()
 
 
-def noise(telescope, bandwidth, intTime, wavelength):
-    '''
-    Purpose
-    -------
-    This provides the expected noise level in Jansky of a telescope,
-    given the observing parameters and properties of the telescope. 
-
-    Inputs
-    ------
-    telescope: class
-        Specifications for a given telescope (beam size, number of antennae)
-    bandwidth: float
-        Bandwidth in Hz
-    intTime: float
-        Integration time in seconds
-
-    wavelength: 
-
-    Returns
-    -------
-    fluxDensityRMS: float
-        The rms in the flux density, in units of Janskys 
-
-
-    Notes
-    -----
-    Maybe supposed to develop more of the variables used here?
-
-    Should incorporate in Telescope class.
-
-    '''
-
-    numSamp = bandwidth * intTime
-    gain = (telescope.effArea / (2 * kb) ) * jansky
-
-    rmsTemp = telescope.sysTemp / math.sqrt(numSamp)
-    rmsFluxDensity = rmsTemp / gain
-
-    return rmsFluxDensity
 
 
