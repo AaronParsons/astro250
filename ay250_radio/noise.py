9c7f73a95ab570e797d16003b986f4a0731b80fb
James McBride <jmcbride@berkeley.edu>
2011-09-13 21:41:00 -0700

    added telescope classes, where telescope specific variables are stored

--- ay250_radio/noise.py
+++ ay250_radio/noise.py
@@ -3,15 +3,39 @@
 # should predict noise levels for observations of given bandwidth, time,
 # number of antennae, etc.
 
+class Telescope:
+    def __init__(self):
+        self.beamSize = 0
+        self.numAnt = 1
+
+class ATA(Telescope):
+    def __init__(self):
+
+        Telescope.__init__()
+
+class CARMA(Telescope):
+    def __init__(self):
+
+        Telescope.__init__()
 
 
-def noise(bandwidth, intTime, beamSize, wavelength):
+def noise(bandwidth, intTime, wavelength):
     '''
     Purpose
     -------
 
     Inputs
     ------
+    bandwidth: flt
+        Bandwidth in Hz
+    intTime: flt
+        Integration time in seconds
+    beamSize:
+
+    wavelength: 
+
+    numAnt: int
+        Number of antennae. 
 
     Notes
     -----
