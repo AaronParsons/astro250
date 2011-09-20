ca13f82d8f4a9e531659afaf6df1f75fe04622e0
James McBride <jmcbride@berkeley.edu>
2011-09-20 16:02:53 -0700

    finished first pass attempt, will now test

--- radioastro/visibilitySim.py
+++ radioastro/visibilitySim.py
@@ -32,14 +32,26 @@ def twoAntennae(antennaOneCoords, antennaTwoCoords, sourceCoords, frequency):
     '''
 
     # need to calculate the time delay
-    baselineVector = antennaOneCoords - antennaTwoCoords
+    baselineCoordVector = antennaOneCoords - antennaTwoCoords
     sourceUnitVector = sourceCoords / sqrt(sum(sourceCoords**2))
 
     # we have position in equatorial coordinates
     # this translates in to a physical distance along the surface of the Earth
+    # for instance, a separation of pi radians in equatorial position
+    # is a distance of pi * radEarth
+    # this encodes the distance (in centimeters) and the direction
 
+    # we want the shortest path, so interested in range -pi to pi
+    # given array design, this is surely unnecessary
+    if baselineCoordVector > pi:
+        baselineCoordVector -= 2 * pi
+    baselineVector = baselineCoordVector * radEarth
 
+    
     # time delay given by dot product of baseline vector and
     # the unit vector in direction of signal
     timeDelay = baselineVector * sourceUnitVector / c
+    phase = 2 * pi * timeDelay * frequency
+
+    return phase
 
