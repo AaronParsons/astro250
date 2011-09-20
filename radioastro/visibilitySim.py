ce934814b2f5b03c8220e67408cd4cc3c3ba523d
James McBride <jmcbride@berkeley.edu>
2011-09-20 01:25:07 -0700

    added a bit of psuedocode to help get started

--- radioastro/visibilitySim.py
+++ radioastro/visibilitySim.py
@@ -3,8 +3,11 @@
 # and a source (with x,y,z also in equatorial coordinates), 
 # compute the phase that you would measure as a function of frequency.
 
+# AstroConstants must be in path
+from AstroConstants import c
 
-def twoAntennae(antennaOneCoords, antennaTwoCoords, sourceCoords):
+
+def twoAntennae(antennaOneCoords, antennaTwoCoords, sourceCoords, frequency):
     '''
     Purpose
     -------
@@ -13,4 +16,18 @@ def twoAntennae(antennaOneCoords, antennaTwoCoords, sourceCoords):
     the positions of the antennae and source (all in equatorial
     coordinates), as well as the frequency, this will return the phase.
 
+
+
     '''
+
+    # need to calculate the time delay
+
+    # how to calculate baseline and unit vector for source
+    baselineVector = [0., 0.]
+    sourceUnitVector = [0., 0.]
+
+
+    # time delay given by dot product of baseline vector and
+    # the unit vector in direction of signal
+    timeDelay = baselineVector * sourceUnitVector / c
+
