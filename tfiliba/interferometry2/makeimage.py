99957e44f038332a22c60bef7d6e357009066591
Terry Filiba <tfiliba@gmail.com>
2012-11-02 15:22:07 -0700

    Initial code to grid data

---
+++ tfiliba/interferometry2/makeimage.py
@@ -0,0 +1,83 @@
+import aipy
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal
+
+
+#def griddata(data, x, y, delta):
+#    grid = np.zeros([x/delta,y/delta])
+#    for in data:
+#        grid[] = data
+
+
+
+uv = aipy.miriad.UV('sim.uv')
+#print uv['nchan']
+#print uv['sfreq']
+#print uv['sdf']
+
+freq = np.zeros([uv['nchan']])
+#for i in range(uv['nchan']):
+#    freq[i] = uv['sfreq'] + i*uv['sdf']
+
+u = []
+v = []
+uvdata = []
+#grid = np.zeros([x/delta,y/delta])
+for (uvw,t,bl),data,flags in uv.all(raw=True):
+    #uvw*uv['sfreq'] #change coordiate to wavelengths
+    #print uvw
+    u.append(uvw[0]*uv['sfreq'])
+    v.append(uvw[1]*uv['sfreq'])
+    uvdata.append(data[0])
+    #print uvw[0],uvw[1]
+    #for freq in range(uv['nchan']):   
+    #    print uvw, t, bl, uv['pol'], data
+
+umat = np.array(u)
+vmat = np.array(v)
+uvdatamat = np.array(uvdata)
+#plt.plot(umat,vmat,'r+')
+#plt.show()
+
+#grid the data
+#determine the size of the grid
+x = umat.max()-umat.min()
+y = vmat.max()-vmat.min()
+#determine the resolution of the grid
+delta = 2
+#generate an empty grid
+complexgrid = np.zeros([np.ceil(x/delta),np.ceil(y/delta)],dtype=uvdatamat.dtype)
+emptygrid = np.zeros([np.ceil(x/delta),np.ceil(y/delta)])
+
+#put data into the empty grid
+for i in range(uvdatamat.size):
+    xcoord = np.floor((umat[i]-umat.min())/delta)
+    ycoord = np.floor((vmat[i]-vmat.min())/delta)
+    #print xcoord, ycoord, uvdatamat[i]
+    if complexgrid[xcoord,ycoord] == 0:
+        complexgrid[xcoord,ycoord] = uvdatamat[i]
+        emptygrid[xcoord,ycoord] = 1
+    #else:
+    #    print 'oversampled'
+
+#imgplot = plt.imshow(abs(complexgrid))
+#imgplot.set_cmap('hot')
+#plt.show()
+#imgplot = plt.imshow(emptygrid)
+#plt.show()
+#deconvolvedgrid = scipy.signal.convolve2d(complexgrid, emptygrid)
+#plt.imshow(deconvolvedgrid)
+
+# invert the grid
+#inverteddata = np.fft.ifft2(complexgrid)
+#plt.imshow(abs(invertedgrid))
+#plt.show()
+
+        
+        
+        
+
+        
+    
+    
